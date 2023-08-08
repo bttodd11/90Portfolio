2c7db45d4a7c6ef38692e5b83abc5b8e3f76d7fd
bttodd11 <bttodd@icloud.com>
2023-08-08 08:16:29 -0500

    All the APIs are working

--- my-app/server.js
+++ my-app/server.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 let octoId = process.env.REACT_APP_USER_PASSWORD;
 const app = express();
 const octokit = new Octokit({ auth: octoId });
+let repos = 0;
+let language = "";
+let githubLink = "";
+let numberOfCommits = 0
+let languageMap = {};
 
 
 
@@ -24,7 +29,7 @@ app.get('/', async (req, res) => {
 
    const userOptions = {
         method: 'GET',
-        url: 'https://api.github.com/users/bttodd11/',
+        url: 'https://api.github.com/users/bttodd11',
         headers: {
             'X-GitHub-Api-Version': '2022-11-28',
             'Authorization': octoId 
@@ -42,8 +47,33 @@ app.get('/', async (req, res) => {
    }
 
 
-    axios.request(options.url).then((response) => {
-        console.log(response.data.length)
+    axios.request(repoOptions.url).then((response) => {
+        repos = response.data.length;
+
+
+        // Finding the most commonly used languages 
+    for(let index = 0; index < response.data.length; index++){
+
+      let currentLanguage = response.data[index].language;
+
+      if(languageMap[currentLanguage]){
+        languageMap[currentLanguage] += 1
+      }  else {
+        languageMap[currentLanguage] = 1
+      }
+    }
+
+    let highValue = Object.values(languageMap).sort(function(a,b){return b - a});
+   language = Object.keys(languageMap).filter(function(key) {return languageMap[key] === highValue[0]})[0];
+
+    })
+    axios.request(userOptions).then((response) => {
+        githubLink = response.data.html_url;
+    })
+    axios.request(lastYearCommitOptions).then((response) => {
+        if(response.data){
+            numberOfCommits = response.data[0].total;
+        }
     })
 })
 
