ca4bee24e50425fe28f5a4f129c2b21d7b234552
bttodd11 <bttodd@icloud.com>
2023-08-09 08:08:29 -0500

    Finished the calls just need to look into dotenv

--- my-app/src/component/stats/stats.jsx
+++ my-app/src/component/stats/stats.jsx
@@ -6,26 +6,37 @@ import "./stats.css";
 
 let Stats = () => {
 
-  const { Octokit } = require("@octokit/rest");
   let [repos, setRepos] = useState(0)
   let [language, setLanguage] = useState('')
   let [githubLink, setGithubLink] = useState('')
   let [numberOfCommits, setNumberOfCommits] = useState(0)
   let languageMap = {};
 
-  let octoId = process.env.REACT_APP_USER_PASSWORD;
-
-  // const octokit = new Octokit({ auth: octoId });
-
   let getRepo = () => {
 
     const repo = {
       method: 'GET',
-      url: "http://localhost:8000/data",
+      url: "http://localhost:8000/repo",
     }
-    axios.request(options)
+    axios.request(repo)
       .then(function (response) {
-        console.log(response)
+
+         // Finding the most commonly used languages 
+    for(let index = 0; index < response.data.length; index++){
+
+      let currentLanguage = response.data[index].language;
+
+      if(languageMap[currentLanguage]){
+        languageMap[currentLanguage] += 1
+      }  else {
+        languageMap[currentLanguage] = 1
+      }
+    }
+
+    // Find the lannguage of each repo and create a hashMap
+     let highValue = Object.values(languageMap).sort(function(a,b){return b - a});
+     setLanguage(Object.keys(languageMap).filter(function(key) {return languageMap[key] === highValue[0]})[0]);
+     setRepos(response.data.length)
       })
       .catch(function (error) {
         console.error(error);
@@ -33,11 +44,11 @@ let Stats = () => {
 
     const user = {
       method: 'GET',
-      url: "http://localhost:8000/data",
+      url: "http://localhost:8000/user",
     }
-    axios.request(options)
+    axios.request(user)
       .then(function (response) {
-        console.log(response)
+        setGithubLink(response.data.html_url)
       })
       .catch(function (error) {
         console.error(error);
@@ -45,41 +56,17 @@ let Stats = () => {
 
     const lastYearCommits = {
       method: 'GET',
-      url: "http://localhost:8000/data",
+      url: "http://localhost:8000/lastYearCommits",
     }
 
-    
-    axios.request(options)
+
+    axios.request(lastYearCommits)
       .then(function (response) {
-        console.log(response)
+        setNumberOfCommits(response.data[0].total)
       })
       .catch(function (error) {
         console.error(error);
       })
-
-
-    // Finding the most commonly used languages 
-    // for(let index = 0; index < repos.data.length; index++){
-
-    //   let currentLanguage = repos.data[index].language;
-
-    //   if(languageMap[currentLanguage]){
-    //     languageMap[currentLanguage] += 1
-    //   }  else {
-    //     languageMap[currentLanguage] = 1
-    //   }
-    // }
-
-    // Setting Last Year Commits 
-    // setNumberOfCommits(lastYearCommits.data[0].total)
-
-    // Setting Github URL
-    // setGithubLink(user.data.html_url)
-
-    // Find the lannguage of each repo and create a hashMap
-    //  let highValue = Object.values(languageMap).sort(function(a,b){return b - a});
-    //  setLanguage(Object.keys(languageMap).filter(function(key) {return languageMap[key] === highValue[0]})[0]);
-    //  setRepos(repos.data.length)
 
   }
 
