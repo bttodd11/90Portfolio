dcff17594084cc36e02c34891b9c746176f7f57c
bttodd11 <bttodd@icloud.com>
2024-07-30 23:41:37 -0500

    Working on counter

--- my-app/src/component/skills/skills.jsx
+++ my-app/src/component/skills/skills.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import {React, useEffect} from "react";
 import "./skills.css";
 
 
-let skills = () => {
+let Skills = () => {
+  // Start at 0 and have a count effect 
+  let yearNow = new Date().getFullYear()
+  let startNumber = 0;
+  let jsYear = 2014;
+  let javaYear = 2019;
+  let tsYear = 2021;
+  let graphqlYear = 2021 
+  let reactYear = 2016
+  let angularYear = 2018
+  let bootstrapYear = 2016
+  let jestYear = 2022
+
+
+    useEffect(() => {
+      const counters = document.querySelectorAll('.count');
+      const speed = 1;
+
+    
+    
+      counters.forEach((counter) => {
+        const updateCount = () => {
+          const target = parseInt(counter.getAttribute('data-target'));
+          const count = parseInt(counter.innerText);
+          const increment = Math.trunc(target / speed);
+          console.log(increment)
+          if (count < target) {
+            counter.innerText = count + increment;
+            setTimeout(updateCount, 1);
+          } else {
+            counter.innerText = target;
+          }
+        }
+         updateCount();
+      });
+    })
 
   return (
     <div id="skillSection">
@@ -11,17 +46,17 @@ let skills = () => {
         <div className="row">
           <div className="col-sm-12 col-md-6 col-lg-6 skillList">
             <h2 className="skillHeader">Languages</h2>
-            <p>JavaScript: 7yrs</p>
-            <p>Java: 3yrs</p>
-            <p>TypeScript: 1yr</p>
-            <p>GraphQL: 1yr</p>
+            <p className="count" data-target={yearNow - jsYear}>0</p>
+            <p className="count" data-target={yearNow - javaYear}>0</p>
+            {/* <p className="count" data-target={yearNow - tsYear}>0</p>
+            <p className="count" data-target={yearNow - graphqlYear}>0</p>
           </div>
           <div className="col-sm-12 col-md-6 col-lg-6 skillList">
             <h2 className="skillHeader">Frameworks</h2>
-            <p>ReactJS: 5yrs</p>
-            <p>AngularJS: 5yrs</p>
-            <p>Bootstrap: 7yrs</p>
-            <p>Jest: 1yr</p>
+            <p className="count" data-target={yearNow - reactYear}>0</p>
+            <p className="count" data-target={yearNow - angularYear}>0</p>
+            <p className="count" data-target={yearNow - bootstrapYear}>0</p>
+            <p className="count" data-target={yearNow - jestYear}>0</p> */}
           </div>
         </div>
       </div>
@@ -29,4 +64,4 @@ let skills = () => {
   )
 }
 
-export default skills;
+export default Skills;
