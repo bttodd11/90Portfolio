a705fd12ce5fe22ce435829e33b19bbd3bd727ce
bttodd11 <bttodd@icloud.com>
2024-08-02 08:26:18 -0500

    Adding countup instead of creating a function to count

--- my-app/src/component/skills/skills.jsx
+++ my-app/src/component/skills/skills.jsx
@@ -1,5 +1,6 @@
-import {React, useEffect} from "react";
+import {React, useEffect, useRef} from "react";
 import "./skills.css";
+import {CountUp} from "countup.js";
 
 
 let Skills = () => {
@@ -14,49 +15,59 @@ let Skills = () => {
   let angularYear = 2018
   let bootstrapYear = 2016
   let jestYear = 2022
+  const countupRef = useRef(null);
+  let countUpAnim;
+
 
 
     useEffect(() => {
-      const counters = document.querySelectorAll('.count');
-      const speed = 1;
+      
+      initCountUp();
+    },[])
 
-    
-    
-      counters.forEach((counter) => {
-        const updateCount = () => {
-          const target = parseInt(counter.getAttribute('data-target'));
-          const count = parseInt(counter.innerText);
-          const increment = Math.trunc(target / speed);
-          console.log(increment)
-          if (count < target) {
-            counter.innerText = count + increment;
-            setTimeout(updateCount, 1);
-          } else {
-            counter.innerText = target;
-          }
-        }
-         updateCount();
-      });
-    })
+    let initCountUp = async () => {
+      countUpAnim = new CountUp(countupRef.current, 1000);
+      if (!countUpAnim.error) {
+        countUpAnim.start();
+      } else {
+        console.error(countUpAnim.error);
+      }
+    }
 
-  return (
+  return ( 
     <div id="skillSection">
       <h2 className="skillTitle">Tech Savvy</h2>
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-12 col-md-6 col-lg-6 skillList">
             <h2 className="skillHeader">Languages</h2>
-            <p className="count" data-target={yearNow - jsYear}>0</p>
-            <p className="count" data-target={yearNow - javaYear}>0</p>
-            {/* <p className="count" data-target={yearNow - tsYear}>0</p>
-            <p className="count" data-target={yearNow - graphqlYear}>0</p>
+            <div className="spacer">
+            <p style={{display:"inline"}}>JavaScript </p><p className="count" ref={countupRef}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div>
+            {/* <div className="spacer">
+            <p style={{display:"inline"}}>Java </p><p className="count" data-target={yearNow - javaYear}>0</p><p style={{display: "inline"}}> yrs</p>
+            </div>
+            <div className="spacer">
+            <p style={{display:"inline"}}>TypeScript </p><p className="count" data-target={yearNow - tsYear}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div>
+            <div className="spacer">
+            <p style={{display:"inline"}}>GraphQL </p><p className="count" data-target={yearNow - graphqlYear}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div>
           </div>
           <div className="col-sm-12 col-md-6 col-lg-6 skillList">
             <h2 className="skillHeader">Frameworks</h2>
-            <p className="count" data-target={yearNow - reactYear}>0</p>
-            <p className="count" data-target={yearNow - angularYear}>0</p>
-            <p className="count" data-target={yearNow - bootstrapYear}>0</p>
-            <p className="count" data-target={yearNow - jestYear}>0</p> */}
+            <div className="spacer">
+            <p style={{display:"inline"}}>React </p><p className="count" data-target={yearNow - reactYear}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div>
+            <div className="spacer">
+            <p style={{display:"inline"}}>Angular </p><p className="count" data-target={yearNow - angularYear}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div>
+            <div className="spacer">
+            <p style={{display:"inline"}}>Bootstrap </p><p className="count" data-target={yearNow - bootstrapYear}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div>
+            <div className="spacer">
+            <p style={{display:"inline"}}>Jest </p><p className="count" data-target={yearNow - jestYear}>0</p><p style={{display:"inline"}}> yrs</p>
+            </div> */}
           </div>
         </div>
       </div>
