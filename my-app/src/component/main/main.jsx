b5c91ca58791492023cf8d1c3006b0f77fb70361
bttodd11 <bttodd@icloud.com>
2024-08-31 12:04:16 -0500

    Updated experience

--- my-app/src/component/main/main.jsx
+++ my-app/src/component/main/main.jsx
@@ -1,10 +1,31 @@
-import {React} from "react";
+import {React, useEffect} from "react";
 import headshot from "../../img/pixelCharacter.png";
 import "./main.css";
+import {CountUp} from "countup.js";
 
 
 
-const Main = () => {
+
+let Main = () => {
+  let yearNow = new Date().getFullYear()
+  let experience = 2014;
+  let countUpAnim8;
+
+
+  useEffect(() => {
+    initCountUp();
+  },[])
+
+  let initCountUp = async () => {
+
+    countUpAnim8 = new CountUp('experience', yearNow - experience, {enableScrollSpy: true, duration: 5, suffix:" yrs"});
+
+    if (!countUpAnim8.error) {
+      countUpAnim8.start();
+    } else {
+      console.error(countUpAnim8.error);
+    }
+  }
 
   return (
     <div className="mainSection">
@@ -18,7 +39,7 @@ const Main = () => {
           <p className="name"> <span className="arrow">&#10148;</span> Player: <span className="myName"> Brian Todd</span></p>
           <p>Location: <span> South Austin,Texas</span></p>
           <p>Occupation: <span> Software Engineer</span></p>
-          <p>Experience: <span> 7 yrs</span></p>
+          <p>Experience: <span id="experience">0</span></p>
         </div>
           </div>
         </div>
